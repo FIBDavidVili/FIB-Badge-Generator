21ee75cbb649bb5c6d05fda23e26d36b71973cfe
FIBDavidVili <david_vili@outlook.com>
2026-03-24 18:40:42 +0100

    Update badge.ts

--- api/badge.ts
+++ api/badge.ts
@@ -1,7 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import {
-  getBadgeDataFromDiscordId,
-} from "../src/lib/badge";
+import { getBadgeDataFromDiscordId } from "../src/lib/badge.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
